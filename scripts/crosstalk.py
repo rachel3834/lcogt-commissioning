d983268d54c89affed859048878d401908642d87
rachel3834 <rstreet@lcogt.net>
2016-07-20 11:51:36 -0700

    Debugging crosstalk at runtime on operational-support

--- scripts/crosstalk.py
+++ scripts/crosstalk.py
@@ -100,7 +100,7 @@ class CrossImage:
                             4: 0.0
                             }
         self.flux_min = 2000.0
-        self.flux_max = 50000.0       	# Default: 75000.0
+        self.flux_max = 750000.0       	# Default: 75000.0
         self.model = 'linear' 	     # One of { linear, polynomial, broken_power_law }
 	
     def overscan_statistics(self,iquad):
