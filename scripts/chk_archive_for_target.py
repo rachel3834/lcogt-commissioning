68d03972574a83175af8676baa1f12cde2216631
rachel3834 <rstreet@lcogt.net>
2016-08-03 14:41:46 -0700

    Debugging DataCounter updates at runtime

--- scripts/chk_archive_for_target.py
+++ scripts/chk_archive_for_target.py
@@ -10,30 +10,26 @@ import glob
 import archive_access
 from astropy.io import fits
 from shutil import remove
+import DataCounter
 
 def chk_archive_for_target():
     """Function to search the archive for frames on a specific object"""
     
     params = parse_args()
     key_list = [ 'OBJECT' ]
-    
+    nframes = 0
     log = open( path.join(params['out_dir'], 'data_listing.log'), 'r' )
-    total_frames = 0
     for night_dir in params['dir_list']:
-        night = night_dir.split('/')[-2]
-        frames = glob.glob( path.join(night_dir,'raw') )
-        nframes = 0
-        for frame in frames:
-            frame_pars = get_frame_params(params, frame, keywords)
-            if frame_pars['OBJECT'] == params['target']:
-                nframes = nframes + 1
-                red_frame = path.join(night_dir,'banzai')
-                if path.isfile(red_frame) == False:
-                    log.write('Warning: No reduced frame for '+\
-                            path.basename(frame)+'\n')
-        log.write(str(night)+' '+str(nframes)+' for target\n')
-        total_frames = total_frames + nframes
-    log.write('\nTotal frames for '+params['target']+': '+str(total_frames)+'\n')
+        night_counter = data_counter.DataCounter()
+        night_counter.night_dir = path.basename(night_dir)
+        night_counter.calc_nightly_object_total(night_dir,params['out_dir'],\
+                                                params['object_name'])
+        output = night_counter.object_data_summary(object_name)
+        log.write(output+'\n')
+        
+        nframes = nframes + night_counter.nscience_raw
+        
+    log.write('\nTotal frames for '+params['object_name']+': '+str(nframes)+'\n')
     log.close()
     
 
@@ -59,12 +55,12 @@ def parse_args():
         params['top_data_dir'] = raw_input('Please enter the path to the data directory: ')
         params['out_dir'] = raw_input('Please enter the output directory path: ')        
         params['date_search_string'] = raw_input('Please enter the date search string [e.g. yyyymmd?]: ')
-        params['target'] = raw_input('Please enter the targets OBJECT keyword: ')
+        params['object_name'] = raw_input('Please enter the targets OBJECT keyword: ')
     else:
         params['top_data_dir'] = argv[1]
         params['out_dir'] = argv[2]
         params['date_search_string'] = argv[3]
-        params['target'] = argv[4]
+        params['object_name'] = argv[4]
     
     params['dir_list'] = glob.glob(path.join(params['top_data_dir'], \
                                 params['date_search_string']))
--- scripts/data_counter.py
+++ scripts/data_counter.py
@@ -27,6 +27,7 @@ class DataCounter:
                 ' N science (quicklook)='+str(self.nscience_quicklook)+\
                 ' N science (reduced)='+str(self.nscience_reduced)
         return output
+        
     def flats_summary(self):
         if len(self.nflats_per_filter) == 0:
             output = self.night_dir+' no flat fields'
@@ -35,32 +36,43 @@ class DataCounter:
             for f,fcount in self.nflats_per_filter.items():
                 output = output + ' N flats('+f+')='+str(fcount)
         return output
+    
+    def object_data_summary(self,object_name):
+        output =  self.night_dir+' N science (raw)='+str(self.nscience_raw)+\
+                ' N science (quicklook)='+str(self.nscience_quicklook)+\
+                ' N science (reduced)='+str(self.nscience_reduced)+\
+                ' for '+object_name
+        return output
         
-    def calc_nightly_data_totals(self,night_dir,out_dir):
-        """Mothod to calculate the number of frames of different types 
-        available within a single nights data directory
-        """
+    def get_night_frame_lists(self):
         
         raw_path = path.join( night_dir, 'raw' )
         quicklook_path = path.join( night_dir, 'preview' )
         reduced_path = path.join( night_dir, 'processed' )
         
-        raw_frames = glob.glob( path.join( raw_path, '*-[b,d,f,e]00.fits.fz' ) )
-        quicklook_frames = glob.glob( path.join( quicklook_path, '-e11.fits.fz'))
-        reduced_frames = glob.glob( path.join( reduced_path, '-e91.fits.fz'))
+        self.raw_frames = glob.glob( path.join( raw_path, '*-[b,d,f,e]00.fits.fz' ) )
+        self.quicklook_frames = glob.glob( path.join( quicklook_path, '-e11.fits.fz'))
+        self.reduced_frames = glob.glob( path.join( reduced_path, '-e91.fits.fz'))
         self.nraw = len(raw_frames)
+        
+    def calc_nightly_data_totals(self,night_dir,out_dir):
+        """Method to calculate the number of frames of different types 
+        available within a single nights data directory
+        """
+        
+        self.get_night_frame_lists()
         
         key_list = ['FILTER']
         params = {'out_dir':out_dir}
         
-        for frame in raw_frames:
+        for frame in self.raw_frames:
             if '-b00' in frame:
                 self.nbiases = self.nbiases + 1
             elif '-d00' in frame:
                 self.ndarks = self.ndarks + 1
             elif '-f00' in frame:
                 self.nflats = self.nflats + 1
-                keywords= archive_access.get_frame_params(params,frame,key_list)
+                keywords = archive_access.get_frame_params(params,frame,key_list)
                 if keywords['FILTER'] in self.nflats_per_filter.keys():
                     self.nflats_per_filter[keywords['FILTER']] = \
                             self.nflats_per_filter[keywords['FILTER']] + 1
@@ -69,11 +81,11 @@ class DataCounter:
             elif '-e00' in frame:
                 self.nscience_raw = self.nscience_raw + 1
         
-        for frame in quicklook_frames:
+        for frame in self.quicklook_frames:
             if '-e11' in frame:
                 self.nscience_quicklook = self.nscience_quicklook + 1
         
-        for frame in reduced_frames:
+        for frame in self.reduced_frames:
             if '-e91' in frame:
                 self.nscience_reduced = self.nscience_reduced + 1
     
@@ -93,3 +105,30 @@ class DataCounter:
             else:
                 self.nflats_per_filter[f] = fcount
                 
+    def calc_nightly_object_total(self,night_dir,out_dir,object_name):
+        """Method to calculate the number of frames taken of a given
+        object per night"""
+        
+        self.get_night_frame_lists()
+        
+        key_list = ['OBJECT']
+        params = {'out_dir':out_dir}
+        
+        for frame in self.raw_frames:
+            if '-e00' in frame:
+                keywords = archive_access.get_frame_params(params,frame,key_list)
+                if keywords['OBJECT'] == object_name:
+                    self.nscience_raw = self.nscience_raw + 1
+        
+        for frame in self.quicklook_frames:
+            if '-e11' in frame:
+                keywords = archive_access.get_frame_params(params,frame,key_list)
+                if keywords['OBJECT'] == object_name:
+                    self.nscience_quicklook = self.nscience_quicklook + 1
+        
+        for frame in self.reduced_frames:
+            if '-e91' in frame:
+                keywords = archive_access.get_frame_params(params,frame,key_list)
+                if keywords['OBJECT'] == object_name:
+                    self.nscience_reduced = self.nscience_reduced + 1
+        
