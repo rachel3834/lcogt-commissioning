05eecec1c2d977e00d743b2ef2a326041871cd1f
rachel3834 <rstreet@lcogt.net>
2016-08-03 14:42:24 -0700

    Debugging counter apps at runtime

--- scripts/chk_archive_for_target.py
+++ scripts/chk_archive_for_target.py
@@ -51,7 +51,7 @@ def parse_args():
     from the commandline or prompts"""
     
     params = {}
-    if len(argv) != 3:
+    if len(argv) != 5:
         params['top_data_dir'] = raw_input('Please enter the path to the data directory: ')
         params['out_dir'] = raw_input('Please enter the output directory path: ')        
         params['date_search_string'] = raw_input('Please enter the date search string [e.g. yyyymmd?]: ')
