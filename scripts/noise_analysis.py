8dd1bfdb3ca7092fa3e82e8436dea05e251af491
rachel3834 <rstreet@lcogt.net>
2016-08-25 14:25:16 -0700

    Making histogram axes more robust

--- scripts/noise_analysis.py
+++ scripts/noise_analysis.py
@@ -133,7 +133,7 @@ def plot_quadrant_hist(params,logy=True,xrange=None):
     def get_hist_axis_range(data):
         (hist_data,bins) = np.histogram(data,bins=200)
         idx = hist_data > 10
-        print bins[idx]
+        print hist_data[idx], bins[idx]
         
     (image, params) = get_image_data(params)
     plotfmt = [ 'r', 'b', 'm', 'g' ]
