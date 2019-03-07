95e520c9d43a385a78016e87771b58ba9fb27a58
Daniel Harbeck <drharbeck@gmail.com>
2019-03-07 14:34:51 -0800

    documenting crawlgain script via inline comments

--- deploy/crawlgain.sh
+++ deploy/crawlgain.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-noisegaindatabase="noisegain.sqlite"
-webpageoutputdir="gainhistory"
+# Crawls thourgh the LCO archive and attempts to fit gain / readnosie from automatically identified pairs of sky flats
+# and daytime biases.
+# Edit according to your need below.
+# Reqruiements: gnu parallel needs to be installed.
 
-base=/archive/engineering
-cameras="fa?? fl?? fs?? kb??"
-sites="elp cpt lsc ogg coj tfn"
-dates="201811??"
 
-inputselection="*-[bf]00.fits.fz"
 
-NCPU=3
+noisegaindatabase="noisegain.sqlite"   # Output database file to store results.
+webpageoutputdir="gainhistory"         # existing local subdiretory into which an overview html page will be rendered.
+
+
+base=/archive/engineering              # Location of archive mount
+cameras="fa?? fs?? kb??"               # Camera types to crawl.
+sites="elp cpt lsc ogg coj tfn"        # Which sites to crawl
+dates="2018121?"                       # date range selector.
+
+inputselection="*-[bf]00.fits.fz"      # type of images to serach for. need flats and biases here.
+
+NCPU=2                                 # How many CPUs to employ
 
 for site in $sites; do
  for camera in $cameras; do
