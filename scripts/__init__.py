cc4a32d2f54be4ed2ae3da1284825182e0cd1478
Daniel Harbeck <drharbeck@gmail.com>
2019-03-08 15:41:02 -0800

    start of python packaging effort

---
+++ scripts/__init__.py
@@ -0,0 +1,1 @@
+name = "lcogt-commissioning"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+long_description = fh.read()
+
+setuptools.setup(
+    name="lcogt-commissioning",
+    version="0.9",
+    author="Daniel Harbeck",
+    author_email="dharbeck@lco.global",
+    description="Tool to characterize CCD detectors, tailored towards LCO imagers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drhaz/lcogt-commissioning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
