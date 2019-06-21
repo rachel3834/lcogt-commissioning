9ff1f7ecbec8806769bb50e78a2b02e8c5cdf0b8
Daniel Harbeck <drharbeck@gmail.com>
2019-06-21 22:28:48 +0000

    updated craw script

--- deploy/crawlgain.sh
+++ deploy/crawlgain.sh
@@ -12,36 +12,9 @@ webpageoutputdir="gainhistory"         # existing local subdiretory into which a
 
 
 base=/archive/engineering              # Location of archive mount
-cameras="fa?? fs?? kb??"               # Camera types to crawl.
-sites="elp cpt lsc ogg coj tfn"        # Which sites to crawl
-dates="201905??"                       # date range selector.
-
-inputselection="*-[bf]00.fits.fz"      # type of images to serach for. need flats and biases here.
 
 NCPU=30                                # How many CPUs to employ
 
-for site in $sites; do
- for camera in $cameras; do
+time python lcocommissioning/crawl_noisegain.py --ncpu $NCPU --ndays 5 --noreprocessing --loglevel INFO --database ${noisegaindatabase}
 
-  sitecameras=`find ${base}/${site}  -maxdepth 1 -type d -wholename "*/$camera"`
-
-  for sitecamera in $sitecameras; do
-
-   directories=`find "${sitecamera}" -maxdepth 1 -type d  -wholename "*/${dates}" `
-
-   for day in $directories; do
-
-     searchpath=${day}/raw/${inputselection}
-     echo "Searchpath is $searchpath"
-     sem  -j $NCPU python lcocommissioning/noisegainrawmef.py --noreprocessing --loglevel INFO --sortby filterlevel --database ${noisegaindatabase} $searchpath
-
-   done
-
-  done
-
- done
-done
-
-sem --wait
-
-analysgainhistory --outputdir ${webpageoutputdir} --database ${noisegaindatabase}
+time analysgainhistory --outputdir ${webpageoutputdir} --database ${noisegaindatabase}
