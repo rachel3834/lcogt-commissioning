0721060c5ad85a695032718d923ada4e0521e651
Daniel Harbeck <drharbeck@gmail.com>
2020-02-10 14:00:28 -0800

    update crontab to use direct hooks.

--- deploy/crawlgain.sh
+++ deploy/crawlgain.sh
@@ -7,19 +7,17 @@
 
 
 
-noisegaindatabase="noisegain.sqlite"   # Output database file to store results.
+noisegaindatabase="sqlite:///noisegain.sqlite"   # Output database file to store results.
 webpageoutputdir="/home/dharbeck/public_html/gainhistory"         # existing local subdiretory into which an overview html page will be rendered.
 reprocessing="--noreprocessing"
-reprocessing=""
 
 base=/archive/engineering              # Location of archive mount
 
-NCPU=40                                # How many CPUs to employ
 NDAYS=3
 
-time python lcocommissioning/crawl_noisegain.py --ncpu $NCPU --ndays $NDAYS $reprocessing  --cameratype "fa??" --readmode "full_frame" --loglevel INFO --database ${noisegaindatabase}
-time python lcocommissioning/crawl_noisegain.py --ncpu $NCPU --ndays $NDAYS $reprocessing  --cameratype "fa??" --readmode "central_2k_2x2" --loglevel INFO --database ${noisegaindatabase}
-time python lcocommissioning/crawl_noisegain.py --ncpu $NCPU --ndays $NDAYS $reprocessing --cameratype "fs??" --readmode "default" --loglevel INFO --database ${noisegaindatabase}
-time python lcocommissioning/crawl_noisegain.py --ncpu $NCPU --ndays $NDAYS $reprocessing --cameratype "kb??" --readmode "default" --loglevel INFO --database ${noisegaindatabase}
+time crawlnoisegain --ndays $NDAYS $reprocessing  --cameratype "fa??" --readmode "full_frame" --loglevel INFO --database ${noisegaindatabase}
+time crawlnoisegain --ndays $NDAYS $reprocessing  --cameratype "fa??" --readmode "central_2k_2x2" --loglevel INFO --database ${noisegaindatabase}
+time crawlnoisegain --ndays $NDAYS $reprocessing --cameratype "fs??" --readmode "default" --loglevel INFO --database ${noisegaindatabase}
+time crawlnoisegain --ndays $NDAYS $reprocessing --cameratype "kb??" --readmode "default" --loglevel INFO --database ${noisegaindatabase}
 
-time analysgainhistory --outputdir ${webpageoutputdir} --database ${noisegaindatabase}
+time analysegainhistory --outputdir ${webpageoutputdir} --database ${noisegaindatabase}
