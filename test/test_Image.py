fa6d81b18c915a57479a3317139f0b4d2d38d042
Daniel Harbeck <drharbeck@gmail.com>
2021-10-19 16:45:35 -0700

    add test data and first pytest script. include a .dockerignore file as to not include the test data n the docker image and bloat that up

--- test/test_Image.py
+++ test/test_Image.py
@@ -1,3 +1,4 @@
+from astropy.io import fits
 
 from lcocommissioning.common.Image import Image
 import logging
@@ -5,12 +6,23 @@ logging.basicConfig(level=getattr(logging, 'DEBUG'),
                     format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
 
 def test_texp():
-    image = Image ('testdata/coj1m003-fa19-20211018-0010-b00.fits.fz')
+    image = Image ('testdata/coj1m003-fa19-20211018-0010-b00.fits.fz', overscancorrect=True)
     assert image.primaryheader['EXPTIME'] ==0
-    image = Image ('testdata/coj1m003-fa19-20211018-0047-f00.fits.fz')
+    image = Image ('testdata/coj1m003-fa19-20211018-0047-f00.fits.fz', overscancorrect=True)
     assert image.primaryheader['EXPTIME'] == 21.137
-    image = Image ('testdata/coj0m401-sq01-20211018-0163-b00.fits.fz')
+    image = Image ('testdata/coj0m401-sq01-20211018-0163-b00.fits.fz', overscancorrect=True)
     assert image.primaryheader['EXPTIME'] ==0
-    image = Image ('testdata/coj0m401-sq01-20211018-0167-x00.fits.fz')
+    image = Image ('testdata/coj0m401-sq01-20211018-0167-x00.fits.fz', overscancorrect=True)
     assert image.primaryheader['EXPTIME'] ==0.500
 
+
+
+    hdu = fits.open ('testdata/coj1m003-fa19-20211018-0010-b00.fits.fz')
+    image = Image (hdu, overscancorrect=True, alreadyopenedhdu=True)
+    assert image.primaryheader['EXPTIME'] ==0
+
+    hdu = fits.open ('testdata/coj0m401-sq01-20211018-0163-b00.fits.fz')
+    image = Image (hdu, overscancorrect=True, alreadyopenedhdu=True)
+    assert image.primaryheader['EXPTIME'] ==0
+
+test_texp()
--- test/test_noisegain.py
+++ test/test_noisegain.py
@@ -2,4 +2,3 @@
 
 
 
-sqdataset = []
