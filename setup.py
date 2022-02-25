9bf712d8abcb5facb5d29f7373e94d26965fcf91
Daniel Harbeck <drharbeck@gmail.com>
2022-02-24 17:52:22 -0800

    update  prep for open search update, 2.1.0 release prep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lcocommissioning",
-    version="2.0.16RC",
+    version="2.1.0",
     author="Daniel Harbeck",
     author_email="dharbeck@lco.global",
     description="Tool to characterize CCD detectors and other commissioning tasks for the LCO observatory.",
